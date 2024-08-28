ac06c2e8cf6b9b003a3f48845233c9ce60c72a9d
Cody <ctappan@Codys-MacBook-Pro.local>
2024-08-28 16:02:32 -0700

    Move IRailComponent to dedicated interface header

--- src/include/RailComponents.h
+++ src/include/RailComponents.h
@@ -3,36 +3,9 @@
 
 #include <vector>
 
+#include "interfaces/IRailComponent.h"
+
 namespace Rail {
-    /**
-     * Rail direction is an arbitrary conctept to outline the direction of travel within the network
-     * 
-     * Traditionally "Up" refers to "towards city center" and "Down" is the opposite
-     */
-    typedef enum Direction {
-        UNKNOWN = 0,
-        UP,
-        DOWN
-    };
-
-    class IComponent {
-        /**
-         *  Called to get the next component(s) in a given direction, ignoring traversal rules
-         *
-         *  @param d The direction in which we are traversing the element
-         *  @return const ITraversableElement& A reference to the element we end on after traversing one step
-         */
-        virtual std::vector<const IComponent&> GetNext(Direction d) = 0;
-
-        /**
-         *  Called to traverse the element in the given direction
-         * 
-         *  @param d The direction in which we are traversing the element
-         *  @return const ITraversableElement& A reference to the element we end on after traversing one step
-         */
-        virtual const IComponent& Traverse(Direction d) = 0;
-    };
-
     /**
      * A signal is a component of the rail network which tells trains whether or not they can proceed across
      * an associated connector
